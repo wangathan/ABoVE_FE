5a658d5172816eda3bf86760e89fc0df25f257f1
wangathan <wangathan@gmail.com>
2018-04-11 21:18:31 -0400

    updated combineBlocks to perform imputation and more complete block consolidation

--- combineBlocks.R
+++ combineBlocks.R
@@ -7,15 +7,47 @@
 library(data.table)
 
 featureFiles = list.files("../../data/features/",
-													pattern = "20180118.*bl.*",
+													pattern = "20180411.*bl.*",
 													full.names=T)
 
 featuresdt = lapply(featureFiles, fread)
 featuresdt = rbindlist(featuresdt, use.names=T, fill=T)
 #featuresdt[,V1:=NULL]
 
+# include toolik lake and chasmer fens
+tl = fread("../../data/training_coefs/training_TL_coefs.csv")
+lc = fread("../../data/training_coefs/training_LC_coefs.csv")
+# drop the coefficients
+toDrop = "a1|b1|c1|a0|a2|b2|a3|b3|magnitude"
+dropNames = names(lc)[grep(toDrop, names(lc))]
+tl[, (dropNames) := NULL]
+lc[, (dropNames) := NULL]
+
+featuresdt = rbindlist(list(featuresdt, tl, lc), fill=T)
+featuresdt[,c("i.yr_end", "V1", "i.px", "i.py"):=NULL]
+
+# try imputing here, by ecozone
+for(n in names(featuresdt)[!grepl("tile|set|pixel|ecozone", names(featuresdt))]){
+
+  if(class(unlist(featuresdt[,n,with=F]))=="integer")
+    featuresdt[,(n) := as.numeric(get(n))]
+  featuresdt[, (n) := ifelse(is.na(get(n)), median(get(n),na.rm=T),get(n)), by=ecozone]
+
+}
+
+getMode = function(x){
+
+  tb = table(x)
+  index = which.max(tb)
+  return(names(tb)[index])
+
+}
+
+# impute ecozone using the tiles
+featuresdt[, ecozone := ifelse(is.na(ecozone), getMode(ecozone), ecozone),by = tile]
+
 #featuresdt[, names(featuresdt)[grepl("NaN",names(featuresdt))]:=NULL]
 
-write.csv(featuresdt,"../../data/features/featureSet_20180119_coreSample.csv")
+write.csv(featuresdt,"../../data/features/featureSet_20180411_coreSample.csv")
 
 
